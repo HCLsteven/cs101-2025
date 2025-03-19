92c1cc8a9123923a9139566dc1d2f70a605afea7
HCLsteven <5862046@gmail.com>
2025-03-19 10:44:43 +0800

    Update main2.c

--- hw2/main2.c
+++ hw2/main2.c
@@ -1,13 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
-
 int main()
 {
-    FILE *fp;
-    FILE * fpr;
-    char m_read[1000];
-    fp = fopen("main2.txt", "w+");
-    fpr = fopen("main.c", "r");
+    char m_read[800];
+    FILE*fp=fopen("main.txt", "w");
+    FILE*fpr=fopen("main.c", "r");
     fread(m_read, sizeof(m_read), 1, fpr);
     fprintf(fp, "%s", m_read);
     fclose(fp);
