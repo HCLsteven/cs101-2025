5e623c159f0ef474eb680980c3e1f8f5e9374173
HCLsteven <5862046@gmail.com>
2025-05-20 17:52:01 +0800

    Create tb1.cpp

---
+++ CPP/tb1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter number:";
+    cin >> x;
+    while (x < 1 || x > 4){
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+        }
+        cout << "Invalid number ..." << endl
+        << "try again:";
+        cin >> x;
+    }
+    return 0;
+}
