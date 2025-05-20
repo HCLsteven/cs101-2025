6cf866b7425080e9f1f2e9ebaa99431992a9c0c8
HCLsteven <5862046@gmail.com>
2025-05-20 18:23:34 +0800

    Create cs_7_28.cpp

---
+++ CPP/cs_7_28.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string first_name = "IU";
+    string last_name = " Lee";
+    string full_name = first_name.append(last_name);
+    cout << full_name;
+    return 0;
+}
