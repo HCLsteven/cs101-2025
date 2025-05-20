797f10659ca41ff9c5efc7538a1e9357114ddcf6
HCLsteven <5862046@gmail.com>
2025-05-20 18:04:55 +0800

    Create cs_7_21.cpp

---
+++ CPP/cs_7_21.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter 2 numbers: ";
+    cin >> y >> z;
+    cout << "Sum = " << (x+y+z);
+    return 0;
+}
